8cd16af604c8997cb7d5559a16538e9905a59bab
Manish Chavan <manishc1@umbc.edu>
2012-11-23 16:22:11 -0500

    push implementation

--- spawner.erl
+++ spawner.erl
@@ -1,65 +1,196 @@
 -module(spawner).
 
--export([start/0 , process/1 , find_max/0 , find_min/0 , find_avg/0]).
+-export([start/0 , process/2 , findMax/0 , calculateMax/0 , find_min/0 , find_avg/0]).
 
 
 %% # of processes
--define(LIMIT , 5).
+-define(LIMIT , 10000).
 
 %% Timeout of receive 
--define(TIMEOUT , 2*1000).
+-define(TIMEOUT , 500).
 
 %% Interval for each Gossip Iteration
--define(INTERVAL , 3*1000).
+-define(INTERVAL , 1000).
+
+
+findMax(X , Y) ->
+	if
+		X > Y ->
+		        Max = X;
+		true ->
+			Max = Y
+	end,
+	Max.
+
+selectNeighbor() ->
+	MyNeighbors = get('neighborList'),
+	random:seed(now()),
+	lists:nth(random:uniform(length(MyNeighbors)) , MyNeighbors).
+
+
+%% change according to the secrets
+update(NeighborSecret) ->
+	MySecret = get('secret'),
+	HisSecret = lists:last(NeighborSecret),
+	{_ , HisValue} = HisSecret,
+	
+	if
+		MySecret == (undefined) ->
+			MyNumber = get('number'),
+			Max = findMax(HisValue,MyNumber);
+		true ->
+			MySingleSecret = lists:nth(1 , MySecret),
+			{_ , MyValue} = MySingleSecret,
+			Max = findMax(HisValue,MyValue)
+	end,
+
+	MyNewSecret = [{max , Max}],	
+	io:format("putting |~p|~n",[Max]),
+	put(secret , (MyNewSecret)).
+	
+
+waitPushResponse() ->
+	receive
+
+		{push_response , NeighborSecret} ->
+		        update(NeighborSecret)
+
+	after ?TIMEOUT ->
+	      	io:format("")
+	end.
+
+
+push() ->
+	Neighbor = selectNeighbor(),
+	Secret = get('secret'),
+	Name = get('name'),
+	Neighbor ! {push_request , Secret , Name},
+	waitPushResponse().
+
+
+waitPullComplete() ->
+	receive
+
+		{pull_complete , NeighborSecret} ->
+			update(NeighborSecret)
+
+	after ?TIMEOUT ->
+	      	io:format("")
+
+	end.
+
+
+waitPullResponse() ->
+	receive
+
+		{pull_response , NeighborSecret , Neighbor} ->
+			update(NeighborSecret),
+			Neighbor ! {pull_complete , get('secret')};
+
+		 no_secret ->
+		 	io:format("no secret~n")
+
+	after ?TIMEOUT ->
+		io:format("")
+
+	end.
+
+
+pull() ->
+	Neighbor = selectNeighbor(),
+	Name = get('name'),
+	io:format("pull | ~p | | ~p |~n",[Name,Neighbor]),
+	%%Neighbor ! {pull_request , Name},
+	io:format("before calling neighbor~n"),
+	waitPullResponse().
+
+
+pushpull() ->
+       Secret = get('secret'),
+       if
+		Secret /= (undefined) ->
+		       push();
+		true ->
+		     io:format("")
+       end.
+       %%pull().
 
 
 listen() ->
 
-       {_ , Me} = process_info( self() , registered_name ),
+       pushpull(),
 
        receive
 
-		{"Your Successor", Successor} ->
-		       io:format(" 	      [~p] -> [~p]~n",[Me , Successor]),
-		       listen();
+		%%wake_up ->
+		%%	io:format("[~p] will do gossip~n", [Me]),
+		%%	erlang:send_after(?INTERVAL , Me , wake_up),
+		%%	listen();
 
-		{"Your Predecessor", Predecessor} ->
-		       io:format("[~p] -> [~p]~n",[Predecessor, Me]),
-		       listen();
+		{push_request , NeighborSecret , Neighbor} ->
+			update(NeighborSecret),
+			Neighbor ! {push_response , get('secret')},
+			listen();
 
-		wake_up ->
-			io:format("[~p] will do gossip~n", [Me]),
-			erlang:send_after(?INTERVAL , Me , wake_up),
+		{pull_request , Neighbor} ->
+			io:format("| ~p | pull request from | ~p |~n",[get('name') , Neighbor]),
+			Secret = get('secret'),
+			if
+				Secret ==(undefined) ->
+				       	Neighbor ! no_secret;
+				true ->
+					Neighbor ! {pull_response , get('secret') , get('name')},
+					waitPullComplete()
+			end,
 			listen();
 
 		find_max ->
-			io:format("Ok, i will start finding max"),
-			erlang:send_after(?INTERVAL , Me , wake_up),
+			Secret = get('secret'),
+			if
+				Secret == (undefined) ->
+				       NewSecret = [{max,get('number')}];
+				true ->
+				       NewSecret = [{max,get('number')} | get('secret')]
+			end,
+			put(secret , (NewSecret)),
 			listen();
 
 		find_min ->
-			io:format("Ok, i will start finding min"),
-			erlang:send_after(?INTERVAL , Me , wake_up),
+			Secret = get('secret'),
+			if
+				Secret == (undefined) ->
+				       NewSecret = [{min,get('number')}];
+				true ->
+				       NewSecret = [{min,get('number')} | get('secret')]
+			end,
+			put(secret , (NewSecret)),
+			io:format("Secret|~p|~n",[get('secret')]),
+			%%lang:send_after(?INTERVAL , Me , wake_up),
 			listen();
 
 		find_avg ->
 			io:format("Ok, i will start finding avg"),
-			erlang:send_after(?INTERVAL , Me , wake_up),
-			listen()
+			%%lang:send_after(?INTERVAL , Me , wake_up),
+			listen();
 
-	after ?INTERVAL ->
+		_Others ->
+			io:format("")
+
+	after ?INTERVAL*2 ->
 	      listen()
 
 	end.
 
 
-process(NeighborList) ->
+init_dict(MyNumber, NeighborList) ->
+	put(number , (MyNumber)),
+	{_ , Me} = process_info( self() , registered_name ),
+	put(name , (Me)),
+	put(neighborList , (NeighborList)).
 
-	{_, Me} = process_info( self() , registered_name ),
-	[Predecessor, Successor] = NeighborList,
+process(MyNumber , NeighborList) ->
 
-	%%Predecessor ! {"Your Successor", Me},
-	%%Successor ! {"Your Predecessor", Me},
+	init_dict(MyNumber , NeighborList),
 
 	%%erlang:send_after(?INTERVAL , Me , wake_up),
 	listen().
@@ -77,16 +208,24 @@ do_spawn(0) ->
 
 do_spawn(N) ->
 	ProcessName = list_to_atom( string:concat( "process" , integer_to_list( ?LIMIT - N ) ) ),
-	register(ProcessName , spawn(?MODULE , process , [getRingNeighborList( ?LIMIT - N )])),
+	register(ProcessName , spawn(?MODULE , process , [(?LIMIT - N) , getRingNeighborList( ?LIMIT - N )])),
 	do_spawn(N-1).
 
 
 start() ->
-	do_spawn(?LIMIT).
+	%%try
+		do_spawn(?LIMIT).
+	%%catch
+		%%io:format("")
+	%%end.
 
 
-find_max() ->
-	process1 ! find_max.
+calculateMax() ->
+	process0 ! find_max,
+	exit(self() , "end of purpose").
+
+findMax() ->
+	spawn(?MODULE , calculateMax , []).
 
 
 find_min() ->
